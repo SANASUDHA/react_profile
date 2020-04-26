fc63127cc06346d541b625fbbf56a7ff89250530
sanasudha <sudha139k@gmail.com>
2020-04-26 08:17:13 -0700

    Updates

---
+++ precache-manifest.ea00f548db3c50a525e4f13c034b0703.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20ff3c703cb63b8639aed3fecb3712e9",
+    "url": "/react_profile/index.html"
+  },
+  {
+    "revision": "2698dfe077f2f3750759",
+    "url": "/react_profile/static/css/main.12169ed9.chunk.css"
+  },
+  {
+    "revision": "a428ca1e6e3a170824cc",
+    "url": "/react_profile/static/js/2.c7b80483.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react_profile/static/js/2.c7b80483.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2698dfe077f2f3750759",
+    "url": "/react_profile/static/js/main.dcd60b0a.chunk.js"
+  },
+  {
+    "revision": "69396a6976dfc482db2d",
+    "url": "/react_profile/static/js/runtime-main.d7f80a48.js"
+  }
+]);
